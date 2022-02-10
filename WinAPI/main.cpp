26039f4b4df82dc54aaa0b0ff9d9ac1465319db3
VyacheslavKuzntesov <kuznetsov1997Vyacheslav@yandex.ru>
2022-02-10 23:19:41 +0300

    DZ

--- WinAPI/main.cpp
+++ WinAPI/main.cpp
@@ -1,7 +1,8 @@
 ﻿#include<Windows.h>
 #include"resource.h"
 
-CONST CHAR INVITATION[] = "Введите логин";
+CONST CHAR INVITATION_LOGIN[] = "Введите логин";
+CONST CHAR INVITATION_PASSWORD[] = "Введите пароль";
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -9,9 +10,9 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 {
 	//MessageBox(FindWindow(NULL,"WinAPI - Microsoft Visual Studio Preview"), "Привет! Это мое первое окно сообщения.", "Hello World",
 	//	MB_ICONWARNING | MB_YESNOCANCEL | MB_HELP | MB_DEFBUTTON3 | MB_SYSTEMMODAL);
-	
+
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
-	
+
 	return 0;
 }
 
@@ -25,8 +26,10 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 		//Установка фокуса:
 		//SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETFOCUS, NULL, 0);
+		SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETTEXT, 0, (LPARAM)INVITATION_LOGIN);
+		SendMessage(GetDlgItem(hwnd, IDC_EDIT_PASSWORD), WM_SETTEXT, 0, (LPARAM)INVITATION_PASSWORD);
 	}
-		break;
+	break;
 	case WM_COMMAND:		//Обработка команд нажатия кнопок и т.д.
 	{
 		//WORD = 2BYTES
@@ -35,20 +38,38 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		//HIWORD - два старших Байта в двойном слове
 		switch (LOWORD(wParam))
 		{
-			case IDC_BTN_COPY:
+		case IDC_BTN_COPY:
+		{
+			CONST INT SIZE = 256;
+			CHAR buffer[SIZE]{};
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_LOGIN);					//Получаем HWND элемента окна по его ResourceID
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)buffer);
+			SendMessage(GetDlgItem(hwnd, IDC_EDIT_PASSWORD), WM_SETTEXT, 0, (LPARAM)buffer);
+		}
+		break;
+		case IDOK:	   MessageBox(hwnd, "Была нажата кнопка ОК", "Info", MB_ICONINFORMATION | MB_OK); break;
+		case IDCANCEL: EndDialog(hwnd, 0); break;
+		case IDC_EDIT_LOGIN:
+		{
+			CONST INT SIZE = 256;
+			CHAR buffer[SIZE]{};
+			SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_GETTEXT, SIZE, (LPARAM)buffer);
+			switch (HIWORD(wParam))
 			{
-				CONST INT SIZE = 256;
-				CHAR buffer[SIZE]{};
-				HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_LOGIN);					//Получаем HWND элемента окна по его ResourceID
-				SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)buffer);
-				SendMessage(GetDlgItem(hwnd, IDC_EDIT_PASSWORD), WM_SETTEXT, 0, (LPARAM)buffer);
+			case EN_SETFOCUS:
+				if (strcmp(buffer, INVITATION_LOGIN) == 0)
+					SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETTEXT, 0, (LPARAM)"");
+				break;
+			case EN_KILLFOCUS:
+				if (strcmp(buffer, "") == 0)
+					SendMessage(GetDlgItem(hwnd, IDC_EDIT_LOGIN), WM_SETTEXT, 0, (LPARAM)INVITATION_LOGIN);
+				break;
 			}
-			break;
-			case IDOK:	   MessageBox(hwnd, "Была нажата кнопка ОК", "Info", MB_ICONINFORMATION | MB_OK); break;
-			case IDCANCEL: EndDialog(hwnd, 0); break;
+		}
+		break;
 		}
 	}
-		break;
+	break;
 	case WM_CLOSE:			//Закрытие окна
 		EndDialog(hwnd, 0);
 	}
