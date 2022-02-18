4f4f7dbb70cc98730d58c2198e0ad2bafd9268bc
VyacheslavKuzntesov <kuznetsov1997Vyacheslav@yandex.ru>
2022-02-18 15:53:58 +0300

    Calculator face DONE

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -2,13 +2,19 @@
 #include<Windows.h>
 #include"resource.h"
 
-CONST UINT WINDOW_WIDTH = 260;
-CONST UINT WINDOW_HEIGHT = 330;
+CONST UINT start_x = 10;
+CONST UINT start_y = 70;
+
 CONST UINT i_btn_size = 50;
 CONST UINT interval = 2;
 
+CONST UINT WINDOW_WIDTH = 293;
+CONST UINT WINDOW_HEIGHT = 324;
+
 CONST CHAR g_szClassName[] = "Calculator";
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+//#define DZ
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -68,6 +74,9 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
+		//////////////////////////////////////////////////
+		///////////////		Create 	display		//////////
+		//////////////////////////////////////////////////
 	case WM_CREATE:
 	{
 		HWND hEdit = CreateWindowEx
@@ -88,8 +97,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		///////////////		Create buttons		//////////
 		//////////////////////////////////////////////////
 
-		UINT	start_x = 10;
-		UINT	start_y = 70;
+		/*UINT	start_x = 10;
+		UINT	start_y = 70;*/
 		/*UINT	i_btn_size = 50;
 		UINT	interval = 2;*/
 
@@ -113,102 +122,152 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				digit++;
 			}
 		}
+		//									Zero & Point
+		CreateWindowEx
+		(
+			NULL, "BUTTON", "0", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			start_x, start_y + (i_btn_size + interval) * 3,
+			i_btn_size * 2 + interval, i_btn_size,
+			hwnd, (HMENU)(IDC_BTN_0), GetModuleHandle(NULL), NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "BUTTON", ".", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			start_x + (i_btn_size + interval) * 2, start_y + (i_btn_size + interval) * 3,
+			i_btn_size, i_btn_size,
+			hwnd, (HMENU)(IDC_BTN_POINT), GetModuleHandle(NULL), NULL
+		);
+
+		//									Operations:
+
+		CreateWindowEx
+		(
+			NULL, "BUTTON", "/", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			start_x + (i_btn_size + interval) * 3, start_y,
+			i_btn_size, i_btn_size,
+			hwnd, (HMENU)(IDC_BTN_DIVISION), GetModuleHandle(NULL), NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "BUTTON", "*", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			start_x + (i_btn_size + interval) * 3, start_y + i_btn_size + interval,
+			i_btn_size, i_btn_size,
+			hwnd, (HMENU)(IDC_BTN_MULTIPLICATION), GetModuleHandle(NULL), NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "BUTTON", "-", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			start_x + (i_btn_size + interval) * 3, start_y + (i_btn_size + interval)*2,
+			i_btn_size, i_btn_size,
+			hwnd, (HMENU)(IDC_BTN_MINUS), GetModuleHandle(NULL), NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "BUTTON", "+", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			start_x + (i_btn_size + interval) * 3, start_y + (i_btn_size + interval) * 3,
+			i_btn_size, i_btn_size,
+			hwnd, (HMENU)(IDC_BTN_PLUS), GetModuleHandle(NULL), NULL
+		);
+
+		CreateWindowEx
+		(
+			NULL, "BUTTON", "C", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			start_x + (i_btn_size + interval) * 4, start_y,
+			i_btn_size, i_btn_size * 2 + interval,
+			hwnd, (HMENU)(IDC_BTN_CLEAR), GetModuleHandle(NULL), NULL
+		);
+		CreateWindowEx
+		(
+			NULL, "BUTTON", "C", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			start_x + (i_btn_size + interval) * 4, start_y + (i_btn_size + interval) * 2,
+			i_btn_size, i_btn_size * 2 + interval,
+			hwnd, (HMENU)(IDC_BTN_EQUAL), GetModuleHandle(NULL), NULL
+		);
 	}
 	break;
 	case WM_COMMAND:
 	{
+
+#ifdef DZ
+		CONST INT SIZE = 256;
+		CHAR buffer[SIZE]{};
+		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+
 		if ((LOWORD(wParam) == IDC_BTN_0) && (HIWORD(wParam) == BN_CLICKED))
 		{
-			CONST INT SIZE = 256;
-			CHAR buffer[SIZE]{};
-			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
 			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)buffer);
-			strcat(buffer,"0");
+			strcat(buffer, "0");
 			SendMessage(GetDlgItem(hwnd, IDC_EDIT), WM_SETTEXT, 0, (LPARAM)buffer);
 		}
 		if ((LOWORD(wParam) == IDC_BTN_1) && (HIWORD(wParam) == BN_CLICKED))
 		{
-			CONST INT SIZE = 256;
-			CHAR buffer[SIZE]{};
-			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
 			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)buffer);
 			strcat(buffer, "1");
 			SendMessage(GetDlgItem(hwnd, IDC_EDIT), WM_SETTEXT, 0, (LPARAM)buffer);
 		}
 		if ((LOWORD(wParam) == IDC_BTN_2) && (HIWORD(wParam) == BN_CLICKED))
 		{
-			CONST INT SIZE = 256;
-			CHAR buffer[SIZE]{};
-			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
 			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)buffer);
 			strcat(buffer, "2");
 			SendMessage(GetDlgItem(hwnd, IDC_EDIT), WM_SETTEXT, 0, (LPARAM)buffer);
 		}
 		if ((LOWORD(wParam) == IDC_BTN_3) && (HIWORD(wParam) == BN_CLICKED))
 		{
-			CONST INT SIZE = 256;
-			CHAR buffer[SIZE]{};
-			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
 			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)buffer);
 			strcat(buffer, "3");
 			SendMessage(GetDlgItem(hwnd, IDC_EDIT), WM_SETTEXT, 0, (LPARAM)buffer);
 		}
 		if ((LOWORD(wParam) == IDC_BTN_4) && (HIWORD(wParam) == BN_CLICKED))
 		{
-			CONST INT SIZE = 256;
-			CHAR buffer[SIZE]{};
-			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
 			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)buffer);
 			strcat(buffer, "4");
 			SendMessage(GetDlgItem(hwnd, IDC_EDIT), WM_SETTEXT, 0, (LPARAM)buffer);
 		}
 		if ((LOWORD(wParam) == IDC_BTN_5) && (HIWORD(wParam) == BN_CLICKED))
 		{
-			CONST INT SIZE = 256;
-			CHAR buffer[SIZE]{};
-			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
 			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)buffer);
 			strcat(buffer, "5");
 			SendMessage(GetDlgItem(hwnd, IDC_EDIT), WM_SETTEXT, 0, (LPARAM)buffer);
 		}
 		if ((LOWORD(wParam) == IDC_BTN_6) && (HIWORD(wParam) == BN_CLICKED))
 		{
-			CONST INT SIZE = 256;
-			CHAR buffer[SIZE]{};
-			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
 			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)buffer);
 			strcat(buffer, "6");
 			SendMessage(GetDlgItem(hwnd, IDC_EDIT), WM_SETTEXT, 0, (LPARAM)buffer);
 		}
 		if ((LOWORD(wParam) == IDC_BTN_7) && (HIWORD(wParam) == BN_CLICKED))
 		{
-			CONST INT SIZE = 256;
-			CHAR buffer[SIZE]{};
-			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
 			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)buffer);
 			strcat(buffer, "7");
 			SendMessage(GetDlgItem(hwnd, IDC_EDIT), WM_SETTEXT, 0, (LPARAM)buffer);
 		}
 		if ((LOWORD(wParam) == IDC_BTN_8) && (HIWORD(wParam) == BN_CLICKED))
 		{
-			CONST INT SIZE = 256;
-			CHAR buffer[SIZE]{};
-			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
 			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)buffer);
 			strcat(buffer, "8");
 			SendMessage(GetDlgItem(hwnd, IDC_EDIT), WM_SETTEXT, 0, (LPARAM)buffer);
-		}
+	}
 		if ((LOWORD(wParam) == IDC_BTN_9) && (HIWORD(wParam) == BN_CLICKED))
 		{
-			CONST INT SIZE = 256;
-			CHAR buffer[SIZE]{};
-			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
 			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)buffer);
 			strcat(buffer, "9");
 			SendMessage(GetDlgItem(hwnd, IDC_EDIT), WM_SETTEXT, 0, (LPARAM)buffer);
 		}
+#endif // DZ
+
+		if (LOWORD(wParam) >= IDC_BTN_0 && LOWORD(wParam) <= IDC_BTN_9)
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			CHAR sz_digit[2] = {};
+			sz_digit[0] = LOWORD(wParam) - 1000 + 48;
+			strcat(sz_buffer, sz_digit);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
 	}
-		break;
+	break;
 	case WM_CLOSE:
 		DestroyWindow(hwnd);
 		break;
@@ -216,6 +275,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		PostQuitMessage(0);
 		break;
 	default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
-	}
+}
 	return 0;
 }
